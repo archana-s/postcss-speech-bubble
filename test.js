96fd6b48453720efe98a19a31f5b64f4baa169e9
ArchanaS <archana.june@gmail.com>
2016-01-23 22:55:30 -0800

    wrote a few unit tests

--- test.js
+++ test.js
@@ -10,14 +10,45 @@ require.extensions['.css'] = function (module, filename) {
 var inputCSS = require('./test/input.css');
 var outputCSS = require('./test/output.css');
 
+var inputBubbleNotCorrect = require('./test/input_bubbleIncorrect.css');
+var inputBeakerNotSpecified = require('./test/input_bubbleBeakerNotSpecified.css');
+var inputBeakerNotSpecifiedCorrectly = require('./test/input_beakerIncorrect.css');
+var inputBeakerPositionDirectionIncorrect = require('./test/input_beakerPositionDirectionIncorrect.css');
+
 function run(t, input, output, opts = { }) {
     return postcss([ bubble(opts) ]).process(input)
         .then( result => {
+            if (opts.shouldThrowError) {
+              t.fail();
+            }
             t.same(result.css, output);
-            t.same(result.warnings().length, 0);
+            t.same(result.warnings().length, opts.error || 0);
+        })
+        .catch( () => {
+          if (opts.shouldThrowError) {
+            t.pass();
+          }
         });
 }
 
 test('converts bubble values to CSS', t => {
     return run(t, inputCSS.toString(), outputCSS.toString(), { });
+});
+
+test('throws exception when bubble is not specified correctly', t => {
+    return run(t, inputBubbleNotCorrect, '', { shouldThrowError: true });
+});
+
+test('throws exception when bubble-beaker is not specified', t => {
+    return run(t, inputBeakerNotSpecified, '', { shouldThrowError: true });
+});
+
+test('throws exception when bubble-beaker is not specified correctly', t => {
+    return run(t, inputBeakerNotSpecifiedCorrectly, '',
+      { shouldThrowError: true });
+});
+
+test('throws exception when bubble-beaker position/direction is not specified correctly', t => {
+    return run(t, inputBeakerPositionDirectionIncorrect, '',
+      { shouldThrowError: true });
 });
